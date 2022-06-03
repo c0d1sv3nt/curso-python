6ee51b69d6e91f119ef7822fe257922c1cf389f0
Francisco Piña <60955445+c0d1sv3nt@users.noreply.github.com>
2022-06-03 23:58:35 +0000

    Add files via upload

---
+++ tipo_de_datos.py
@@ -0,0 +1,32 @@
+# Los datos manejados en python son 4
+# • boolean: te daran como respuesta si es verdadero o falso. 
+# • int: su traduccion es, numeros enteros y simplemente son numeros enteros
+# • float: su traduccion es, flotante, que son los numeros decimales 
+# • str: su traduccion es, cuerdas, pero es la definicion que se le da a palabras, solo eso texto leible por humanos
+
+# Este ejemplo imprime una salida True ya que efectivamente 8 es mayor que 7
+
+
+print(8>7)
+
+# Este ejemplo imprime una salida False ya que efectivamente 8 es no es mayor que 9
+
+print(8>9)
+
+# Este ejemplo imprime el tipo de dato que es 1981
+
+num = 1981
+print(type(num)) 
+
+# Este ejemplo imprime el tipo de dato que es 2.500
+
+num2 = 2.500
+print(type(num2))
+
+# Este ejemplo imprime el tipo de dato que es "binvenidos al mundo de python"
+
+Variable = "binvenidos al mundo de python"
+print(type(Variable))
+
+# Como te pudiste dar cuenta usamos ciertos tipos de codigo para dar a conocer o mejor dicho "imprimir" los tipos
+# de datos que se manejan en python
