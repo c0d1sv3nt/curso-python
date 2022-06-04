8885e79754753b3bb51240d2afb3fe92b4de24ab
Francisco Piña <60955445+c0d1sv3nt@users.noreply.github.com>
2022-06-04 00:33:32 +0000

    Add files via upload

---
+++ operadores_numericos.py
@@ -0,0 +1,23 @@
+# Los operadores numericos son los que ya conocemos como: suma, resta, multiplicacion
+# y division pero se le agregan potencia, modulo y division entera
+
+8+8
+print(8+8)
+
+9-5
+print(9-5)
+
+10*98
+print(10*98)
+
+10/2
+print(int(10/2))
+
+9**4
+print(9**4)
+
+50%5
+print(50%5)
+
+1234//34
+print(1234//34)
